929311e366e8387eae584585a2810ae533066512
38samin11 <tafhimurrahmansamin@gmail.com>
2025-08-07 20:01:40 +0600

    Update and rename array rev.c to array rev.cpp

---
+++ array rev.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, arr[100000], n, low, pos;
+    cin >> n;
+    for(i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for(i = n - 1; i >= 0; i--)
+    {
+        cout << arr[i];
+    }
+}
+
+
+
+
